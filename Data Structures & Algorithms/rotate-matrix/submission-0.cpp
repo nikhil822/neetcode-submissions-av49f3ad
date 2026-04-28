ccec38ce239b7f8c8cdb06aec857ee633acce93b
Nikhil Kumar Sahu <75385653+nikhil822@users.noreply.github.com>
2026-04-28 23:05:15 +0530

    Add: rotate-matrix - submission-0

---
+++ Data Structures & Algorithms/rotate-matrix/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i = 0; i < n / 2; i++) {
+            for(int j = 0; j < n; j++) {
+                swap(matrix[i][j], matrix[n - i - 1][j]);
+            }
+        }
+
+        // transpose matrix, swap elements of upper half triangle with lower
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+    }
+};
